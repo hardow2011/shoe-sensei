762c89d784155817a915042152ac930c7252e07a
hardow2011 <hardow2011@outlook.com>
2024-07-29 15:06:44 -0400

    feat: added tags to models

--- app/models/model.rb
+++ app/models/model.rb
@@ -21,9 +21,14 @@
 #
 #  fk_rails_...  (collection_id => collections.id)
 #
+# TODO: add activity_list, cushioning_list and support_list to controller
 class Model < ApplicationRecord
   belongs_to :collection
   has_one :brand, through: :collection
+
+  ACTIVITY_OPTIONS = ['running', 'walking', 'standing', 'training and gym']
+  CUSHIONING_OPTIONS = ['low', 'mid', 'high']
+  SUPPORT_OPTIONS = ['neutral', 'stability']
 
   validates :heel_to_toe_drop, :iteration, :name, :weight, :overview, presence: true
   validates :apma_accepted, inclusion: [ true, false ]
@@ -33,4 +38,24 @@ class Model < ApplicationRecord
   validates :weight, numericality: { greater_than_or_equal_to: 0.1 }
   validates :iteration, uniqueness: { scope: :collection }
   validates_presence_of :collection
+
+  validate :tags_validity
+
+  acts_as_taggable_on :activity
+  acts_as_taggable_on :cushioning
+  acts_as_taggable_on :support
+
+  private
+  def tags_validity
+    self.activity_list.each do |tag|
+      errors.add(:activity, "can't include #{tag}") if ACTIVITY_OPTIONS.exclude?(tag)
+    end
+    self.cushioning_list.each do |tag|
+      errors.add(:cushioning, "can't include #{tag}") if CUSHIONING_OPTIONS.exclude?(tag)
+    end
+    self.support_list.each do |tag|
+      errors.add(:support, "can't include #{tag}") if SUPPORT_OPTIONS.exclude?(tag)
+    end
+  end
+
 end
