2b67bf68814539a32ad5acb3dd97b70f0098f30b
hardow2011 <hardow2011@outlook.com>
2024-08-06 00:47:15 -0400

    fix: corrected filter

--- app/helpers/filter_pagination.rb
+++ app/helpers/filter_pagination.rb
@@ -1,7 +1,9 @@
 module FilterPagination
   include AllowedTags
+
+  MODELS_PER_PAGE = 3
   class ModelsFilter
-    attr_reader :filter_list, :models
+    attr_reader :filter_list, :models, :paged_models
     def initialize(selected_activities, selected_cushionings, selected_supports, brands_ids, hide_brand_filter)
       selected_activities = selected_activities || []
       selected_cushionings = selected_cushionings || []
@@ -23,12 +25,6 @@ module FilterPagination
         @models = Model.all
       end
 
-      filter_list[:activities] = build_filter(selected_activities, :activities).sort_by { |activity| activity[0] }
-      filter_list[:supports] =  build_filter(selected_supports, :support) # this is to sort the support tags
-                                  .sort_by { |k, v| AllowedTags::SUPPORT_OPTIONS.find_index(v[:id]) }
-      filter_list[:cushionings] = build_filter(selected_cushionings, :cushioning)
-                                    .sort_by { |k, v| AllowedTags::CUSHIONING_OPTIONS.find_index(v[:id]) }
-
       # Model.where("tags -> 'activities' ?| array[:activities] AND tags -> 'cushioning' ?| array[:cushioning] AND tags -> 'support' ?| array[:support]", activities: ['Road running', 'Training and gym'], cushioning: ['High'], support: ['Neutral'])
       query = ''
       query << "tags -> 'activities' ?| array#{selected_activities.to_s.gsub('"',"'")} "  if selected_activities.any?
@@ -38,6 +34,13 @@ module FilterPagination
       if query.present?
         @models = @models.where(query)
       end
+
+      filter_list[:activities] = build_filter(selected_activities, :activities).sort_by { |activity| activity[0] }
+      filter_list[:supports] =  build_filter(selected_supports, :support) # this is to sort the support tags
+                                  .sort_by { |k, v| AllowedTags::SUPPORT_OPTIONS.find_index(v[:id]) }
+      filter_list[:cushionings] = build_filter(selected_cushionings, :cushioning)
+                                    .sort_by { |k, v| AllowedTags::CUSHIONING_OPTIONS.find_index(v[:id]) }
+
 
       unless @filter_list[:hide_brand_filter]
 
@@ -52,6 +55,8 @@ module FilterPagination
         brand = Brand.find_by(id: brands_ids.first)
         @filter_list[:brands][brand.name] = { id: brand.id, checked: brands_ids.include?(brand.id) }
       end
+
+      @paged_models = @models.each_slice(MODELS_PER_PAGE).to_a
     end
 
     private
