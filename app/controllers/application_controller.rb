a8de5e8a4cda7961e102bdf93bf5b6054120c72c
hardow2011 <hardow2011@outlook.com>
2024-08-11 23:39:47 -0400

    fix: redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(user)
-    user_url(user)
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      admin_home_url
+    else
+      raise 'Attempted log in s not of type User'
+    end
   end
 end
