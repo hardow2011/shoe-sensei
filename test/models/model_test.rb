dea44044b7ba0d3b335b6643ccf87e0db6ac9b8d
hardow2011 <hardow2011@outlook.com>
2024-08-18 20:15:21 -0400

    featL creatd model model tests

--- test/models/model_test.rb
+++ test/models/model_test.rb
@@ -1,7 +1,92 @@
 require "test_helper"
 
 class ModelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @on_cloud_x = collections(:on_cloud_x)
+    @tags = { activities: ['Walking', 'Standing'], cushioning: 1, support: 'Neutral' }
+  end
+  test "valid model" do
+    model = Model.new(name: 'Uncle Iroh', apma_accepted: true, discontinued: false, heel_to_toe_drop: 4, weight: 154.6,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    assert model.valid?
+  end
+
+  test "invalid without name" do
+    model = Model.new(apma_accepted: true, discontinued: false, heel_to_toe_drop: 4, weight: 154.6,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:name]
+  end
+
+  test "invalid without apma_accepted" do
+    model = Model.new(name: 'Uncle Iroh', discontinued: false, heel_to_toe_drop: 4, weight: 154.6,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:apma_accepted]
+  end
+
+  test "invalid without discontinued" do
+    model = Model.new(name: 'Uncle Iroh', apma_accepted: true, heel_to_toe_drop: 4, weight: 154.6,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:discontinued]
+  end
+
+  test "invalid without heel_to_toe_drop" do
+    model = Model.new(name: 'Uncle Iroh', apma_accepted: true, discontinued: false, weight: 154.6,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:heel_to_toe_drop]
+  end
+
+  test "invalid without weight" do
+    model = Model.new(name: 'Uncle Iroh', apma_accepted: true, discontinued: false, heel_to_toe_drop: 4,
+                       tags: @tags, collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:weight]
+  end
+
+  test "invalid without tags" do
+    model = Model.new(name: 'Uncle Iroh', apma_accepted: true, discontinued: false, weight: 154.6, heel_to_toe_drop: 4,
+                       collection: @on_cloud_x)
+    model.save
+    refute model.valid?
+    assert_not_empty model.errors[:activities]
+    assert_not_empty model.errors[:cushioning]
+    assert_not_empty model.errors[:support]
+
+    model.tags = @tags.except(:activities, :cushioning)
+
+    model.save
+    refute model.valid?
+
+    assert_not_empty model.errors[:activities]
+    assert_not_empty model.errors[:cushioning]
+    assert_empty model.errors[:support]
+
+    model.tags = @tags.except(:activities)
+
+    model.save
+    refute model.valid?
+
+    assert_not_empty model.errors[:activities]
+    assert_empty model.errors[:cushioning]
+    assert_empty model.errors[:support]
+
+    model.tags = @tags
+
+    model.save
+    assert model.valid?
+
+    assert_empty model.errors[:activities]
+    assert_empty model.errors[:cushioning]
+    assert_empty model.errors[:support]
+  end
+
 end
