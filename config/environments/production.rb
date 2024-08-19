42e947e95461c0d46a5e93621ae4dafd687c0e84
hardow2011 <hardow2011@outlook.com>
2024-08-18 20:42:18 -0400

    refactor: added TODO

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
+  # TODO: set active storage for prod
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
