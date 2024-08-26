5e6c0b16591b1c0c7253150f837ecdb2eab9a6c4
hardow2011 <hardow2011@outlook.com>
2024-08-25 23:56:46 -0400

    refactor: refactor

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,17 +3,17 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
-  # Clear attached files after system tests
-  # https://guides.rubyonrails.org/active_storage_overview.html#system-tests
+
 
   setup do
     # Necessary for tests to pass, given that the active storage urls now have the local added
     default_url_options[:locale] = I18n.default_locale
   end
 
+  # Clear attached files after system tests
+  # https://guides.rubyonrails.org/active_storage_overview.html#system-tests
   def after_teardown
     super
-
     FileUtils.rm_rf(ActiveStorage::Blob.service.root)
   end
 
