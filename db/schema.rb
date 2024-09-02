4e01277f821bd7c1c8b4217c7bd0011b6c66b08b
hardow2011 <hardow2011@outlook.com>
2024-09-02 13:56:50 -0400

    test: deferred for the fixtures load to work in render without making rails a superuser

--- db/schema.rb
+++ db/schema.rb
@@ -117,7 +117,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_222211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id", deferrable: :deferred
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "collections", "brands"
   add_foreign_key "models", "collections"
