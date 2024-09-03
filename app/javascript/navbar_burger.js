efebba3ed2bceeb4a79fbffd5183028e0d03423e
hardow2011 <hardow2011@outlook.com>
2024-09-02 20:14:13 -0400

    feat: update for mobile navbar

--- app/javascript/navbar_burger.js
+++ app/javascript/navbar_burger.js
@@ -1,34 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.toggleNavbar = function(clickBtn) {
+  // Get the target from the "data-target" attribute
+  const target = clickBtn.dataset.target;
+  const $target = document.getElementById(target);
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-  
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-  
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-        document.querySelector('nav.navbar').classList.toggle('is-active');
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  clickBtn.classList.toggle('is-active');
+  $target.classList.toggle('is-active');
+  document.querySelector('nav.navbar').classList.toggle('is-active');
 
-        document.documentElement.classList.toggle("is-clipped");
-  
-      });
-    });
-  
-  });
+  document.documentElement.classList.toggle("is-clipped");
+}
 
-  window.addEventListener('resize', function(event) {
-    const $htmlTag = document.documentElement;
-    if($htmlTag.classList.contains('is-clipped') && window.screen.width > 768) {
-      document.documentElement.classList.remove('is-clipped');
-      document.querySelector('nav.navbar').classList.remove('is-active');
-      document.querySelector('.navbar-menu').classList.remove('is-active');
-      document.querySelector('.navbar-burger').classList.remove('is-active');
-    }
+// Remove the is-clipped style (if present) from the html tag after page visit
+document.addEventListener('turbo:load', () => {
+  if (document.documentElement.classList.contains("is-clipped")) {
+    document.documentElement.classList.remove("is-clipped");
+  }
+});
+
+window.addEventListener('resize', function(event) {
+  const $htmlTag = document.documentElement;
+  if($htmlTag.classList.contains('is-clipped') && window.screen.width > 768) {
+    document.documentElement.classList.remove('is-clipped');
+    document.querySelector('nav.navbar').classList.remove('is-active');
+    document.querySelector('.navbar-menu').classList.remove('is-active');
+    document.querySelector('.navbar-burger').classList.remove('is-active');
+  }
 }, true);
