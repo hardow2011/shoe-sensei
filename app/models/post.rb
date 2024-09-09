678ceabefeb5c4d11dcb2b88540bf7b09c106de9
hardow2011 <hardow2011@outlook.com>
2024-09-08 20:24:26 -0400

    feat: model tests for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -28,12 +28,14 @@ class Post < ApplicationRecord
   translates :content
   translates :title
 
-  validates :title_en, :title_es, :handle, :tags, presence: true
+  validates :title_en, :title_es, :content_en, :content_es, :overview_en, :overview_es, :handle, :tags, presence: true
   validates :handle, format: { with: DataFormatting::HANDLE_FORMAT }
   validates :published, inclusion: { in: AllowedTags::BOOLEAN_OPTIONS }
-  validates :name, :handle, :title_en, :title_es, uniqueness: true
+  validates :handle, :title_en, :title_es, uniqueness: true
 
   validate :tags_validity
+
+  before_validation :assign_handle
 
   private
 
@@ -41,6 +43,33 @@ class Post < ApplicationRecord
     forbidden_tags = self.tags - AllowedTags::POSTS_OPTIONS
     if forbidden_tags.any?
       errors.add(:tags, "#{forbidden_tags} tags not allowed")
+    end
+  end
+
+  def assign_handle
+    if self.title_en && self.title_en_changed?
+      # .parameterize[0..50] get first 50 chars of parameterized title
+      self.handle = self.title_en.parameterize[0..50]
+
+      # If the handle is longer than 50 chars, remove the last words
+      # .split('-') create array by separating by minus sign
+      # [..-2] take out last element
+      # .join('-') rejoin the array by separating by minus sign
+      while self.handle.length > 50
+        self.handle = self.handle.split('-')[..-2].join('-')
+      end
+
+      if Post.find_by(handle: self.handle).present?
+        # self.handle = self.handle + '-' + index.to_s
+
+        original_handle = self.handle.dup
+        handle_index = 2
+
+        while Post.find_by(handle: self.handle).present? do
+          self.handle = original_handle + '-' + handle_index.to_s
+          handle_index += 1
+        end
+      end
     end
   end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,112 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @valid_post = Post.new(title_en: 'The best looking shoes', title_es: 'Los zapatos más bonitos',
+      overview_en: 'We talk about the prettiest shoes', overview_es: 'hablamos de los zapatos más bonitos',
+      content_en: 'Let\s start by explaining what makes shoes appealing',
+      content_es: 'Empecemos por explicar qué hacen los zapatos visualmente apetecibles',
+      tags: ['road_running', 'walking', 'cost_conscious', 'support'],
+      published: true)
+  end
+  test "valid post" do
+    post = @valid_post
+    post.save
+    assert post.valid?
+  end
+
+  test 'invalid without English title' do
+    post = @valid_post
+    post.title_en = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:title_en], 'no validation for English title present'
+  end
+
+  test 'invalid without Spanish title' do
+    post = @valid_post
+    post.title_es = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:title_es], 'no validation for Spanish title present'
+  end
+
+  test 'invalid without English overview' do
+    post = @valid_post
+    post.overview_en = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:overview_en], 'no validation for English overview present'
+  end
+
+  test 'invalid without Spanish overview' do
+    post = @valid_post
+    post.overview_es = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:overview_es], 'no validation for Spanish overview present'
+  end
+
+  test 'invalid without English content' do
+    post = @valid_post
+    post.content_en = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:content_en], 'no validation for English content present'
+  end
+
+  test 'invalid without Spanish content' do
+    post = @valid_post
+    post.content_es = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:content_es], 'no validation for Spanish content present'
+  end
+
+  test 'invalid without published' do
+    post = @valid_post
+    post.published = nil
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:published], 'no validation for published'
+  end
+
+  test 'correct title parameterization to handle' do
+    post = @valid_post
+    post.save
+    assert post.valid?
+    assert_equal post.handle, 'the-best-looking-shoes', 'handle malformed'
+  end
+
+  test 'correct handle cut-off' do
+    post = @valid_post
+    post.title_en = "I went to the store to get some nw shoes, but they ran out of the models I wanted. So I went home sad"
+    post.save
+    assert post.valid?
+    assert_equal post.handle, 'i-went-to-the-store-to-get-some-nw-shoes-but-they', 'handle malformed'
+
+    new_post = @valid_post
+    new_post.title_en = "I went to the store to get some nw shoes, but they ran out of the models I wanted. So I went home sad Pt.2"
+    new_post.title_es = "No importa"
+    new_post.save
+    assert new_post.valid?
+    assert_equal new_post.handle, 'i-went-to-the-store-to-get-some-nw-shoes-but-they-2', 'handle iteration malformed'
+  end
+
+  test 'unique title' do
+    @valid_post.save
+    post = @valid_post.dup
+    post.save
+    refute post.valid?, 'post valid with non unique title'
+    assert_not_empty post.errors[:title_en], 'no validation for non unique English title'
+    assert_not_empty post.errors[:title_es], 'no validation for non unique Spanish title'
+  end
+
+  test 'invalid with invalid tags' do
+    post = @valid_post.dup
+    post.tags = ['road_running', 'ola_k_ase', 'standing', 'something_else']
+    post.save
+    refute post.valid?
+    assert_not_empty post.errors[:tags], 'no validation for tags'
+  end
 end
