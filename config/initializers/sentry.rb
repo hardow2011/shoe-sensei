9c530264c572ffd06d7b3b89f13f9ae83c983579
hardow2011 <hardow2011@outlook.com>
2024-09-11 15:35:42 -0400

    feat: put sentry dsn in env

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 # TODO: put sentry dsn in secret
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.dsn = "https://9c6332cfe3e4677d0e52cc772d2e14d0@o4506474750869504.ingest.us.sentry.io/4507909763104768"
+  config.dsn = ENV['SENTRY_DSN']
   config.enable_tracing = true
   # TODO: find out what those values are
   config.traces_sample_rate = 1.0
