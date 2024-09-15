40b736f1fbb398bd0f6069b104fa26ec56a15fbe
hardow2011 <hardow2011@outlook.com>
2024-09-15 17:55:29 -0400

    refactor: moved admin specific js to admin layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,8 @@ import "./controllers"
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 
-import './direct_uploads'
 import "@fortawesome/fontawesome-free/js/all";
-import './img_upload_previews'
 import './navbar_burger'
 import './model_filter_mobile'
 import './model_grid_skeleton'
-// import "trix"
-// import "./trix_customization"
 import "@rails/actiontext"
