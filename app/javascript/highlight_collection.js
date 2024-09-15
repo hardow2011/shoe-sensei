fbbb78a34bd8503b5aed7f050cfc696d4b7f1fa9
hardow2011 <hardow2011@outlook.com>
2024-09-15 17:52:03 -0400

    feat: highlight collection in brand page

--- app/javascript/highlight_collection.js
+++ app/javascript/highlight_collection.js
@@ -1,7 +1,10 @@
-const selectedCollection = window.location.hash.substring(1);
-if (selectedCollection) {
-    const collectionHtmlTag = document.getElementById(selectedCollection);
-    if (collectionHtmlTag) {
-        collectionHtmlTag.classList.add('highlighted-background')
+document.addEventListener('turbo:load', function() {
+    const selectedCollection = window.location.hash.substring(1);
+    if (selectedCollection) {
+        console.log(selectedCollection);
+        const collectionHtmlTag = document.getElementById(selectedCollection);
+        if (collectionHtmlTag) {
+            collectionHtmlTag.classList.toggle('highlighted-background')
+        }
     }
-}
+})
