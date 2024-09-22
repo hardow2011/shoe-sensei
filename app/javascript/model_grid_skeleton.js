cfae30b4e7b39d588a50ef2579ed4d3a5e4e396e
hardow2011 <hardow2011@outlook.com>
2024-09-21 22:30:15 -0400

    fix: grid skeleton

--- app/javascript/model_grid_skeleton.js
+++ app/javascript/model_grid_skeleton.js
@@ -1,5 +1,5 @@
 window.submit_filter_form = function (el, skip_form_sending = false) {
-    const modelsFilter = document.getElementById('models-filter');
+    const modelsFilter = document.getElementById('models-filter-form');
     modelsFilter.classList.add('is-loading');
     el.form.requestSubmit();
   }
