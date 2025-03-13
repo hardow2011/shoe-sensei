ede1e1e24c519112e1cfab0f283f4ac0046b23b9
Louvens Raphael <hardow2011@outlook.com>
2025-03-13 18:24:17 -0400

    fix: redirect after sign up confirm

--- app/controllers/users/devise/confirmations_controller.rb
+++ app/controllers/users/devise/confirmations_controller.rb
@@ -22,9 +22,9 @@ class Users::Devise::ConfirmationsController < Devise::ConfirmationsController
   # protected
 
   # The path used after resending confirmation instructions.
-  # def after_resending_confirmation_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_resending_confirmation_instructions_path_for(resource_name)
+    new_user_session_path
+  end
 
   # The path used after confirmation.
   # def after_confirmation_path_for(resource_name, resource)
