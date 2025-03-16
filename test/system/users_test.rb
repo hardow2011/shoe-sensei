b3a342bf1e02501aaa23cb923e279136cb10fd3d
Louvens Raphael <hardow2011@outlook.com>
2025-03-16 11:37:37 -0400

    feat: resenc confirmation email

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -68,6 +68,8 @@ class UsersTest < ApplicationSystemTestCase
 
         click_on 'Sign Up'
 
+        assert_text "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
+
         visit_email_link('Confirm my account')
 
         assert_text 'Your email address has been successfully confirmed.'
@@ -188,5 +190,45 @@ class UsersTest < ApplicationSystemTestCase
 
         assert_no_selector 'a', text: 'Join'
         assert_selector 'a', text: 'Account'
+    end
+
+    test 'resend confirmation instructions' do
+        visit root_url
+
+        assert_selector 'a', text: 'Join'
+        click_on 'Join'
+
+        assert_text "Don't have an account yet?"
+        assert_no_text "Already have an account?"
+        assert_selector 'a', text: 'Sign Up'
+        click_on 'Sign Up'
+
+        assert_text "Already have an account?"
+        assert_no_text "Don't have an account yet?"
+        fill_in "user[username]", with: @new_user[:username]
+        fill_in "user[email]", with: @new_user[:email]
+        fill_in "user[password]", with: @new_user[:password]
+        fill_in "user[password_confirmation]", with: @new_user[:password]
+
+        click_on 'Sign Up'
+
+        assert_text "A message with a confirmation link has been sent to your email address. Please follow the link to activate your account."
+
+        assert_selector 'a', text: 'Join'
+        click_on 'Join'
+
+        assert_selector 'a', text: "Didn't receive confirmation instructions?"
+        click_on "Didn't receive confirmation instructions?"
+
+        fill_in "user[email]", with: @new_user[:email]
+        click_on "Resend confirmation instructions"
+
+        assert_text "You will receive an email with instructions for how to confirm your email address in a few minutes."
+
+        visit_email_link('Confirm my account')
+
+        assert_text "Your email address has been successfully confirmed."
+
+        login(email: @new_user[:email], password: @new_user[:password])
     end
 end
