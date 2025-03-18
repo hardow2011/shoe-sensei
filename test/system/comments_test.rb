73af13d5171b28adafcce49d68c3817be602d47d
Louvens Raphael <hardow2011@outlook.com>
2025-03-17 20:59:23 -0400

    wip: comments and replies

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -15,9 +15,39 @@ class CommentsTests < ApplicationSystemTestCase
 
         assert_no_text comment_content
 
+        # Scroll down to activate the lazy loaded comments.
+        # Sleep is necessary because scroll is async.
+        page.scroll_to(0, 5000)
+        sleep 1
+
         find('[name="comment[content]"]').set(comment_content)
 
         click_on 'Post Comment'
+
+        assert_text 'Comment posted successfully.'
+        assert_text comment_content
+    end
+
+    test 'reply to comment' do
+        comment_content = 'Very good information'
+        login(email: @user.email, password: 'An4r!s+Q')
+
+        click_on 'Blog'
+
+        click_on 'Read more...'
+
+        assert_no_text comment_content
+
+        # Scroll down to activate the lazy loaded comments.
+        # Sleep is necessary because scroll is async.
+        page.scroll_to(0, 5000)
+        sleep 1
+
+        click_on 'Reply'
+
+        find('.comment-reply-input').set(comment_content)
+
+        click_on 'Post Reply'
 
         assert_text 'Comment posted successfully.'
         assert_text comment_content
