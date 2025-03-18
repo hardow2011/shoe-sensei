ab8f580f8c5aa38834c2e2255bf5c8d9a72aed97
Louvens Raphael <hardow2011@outlook.com>
2025-03-17 20:59:29 -0400

    wip: comments and replies

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -3,40 +3,39 @@ require 'application_system_test_case'
 class CommentsTests < ApplicationSystemTestCase
     setup do
         @user = users(:andris)
+        @comment_content = 'Very good information'
     end
     
     test 'post comment under post' do
-        comment_content = 'Very good information'
         login(email: @user.email, password: 'An4r!s+Q')
 
         click_on 'Blog'
 
         click_on 'Read more...'
 
-        assert_no_text comment_content
+        assert_no_text @comment_content
 
         # Scroll down to activate the lazy loaded comments.
         # Sleep is necessary because scroll is async.
         page.scroll_to(0, 5000)
         sleep 1
 
-        find('[name="comment[content]"]').set(comment_content)
+        find('[name="comment[content]"]').set(@comment_content)
 
         click_on 'Post Comment'
 
         assert_text 'Comment posted successfully.'
-        assert_text comment_content
+        assert_text @comment_content
     end
 
     test 'reply to comment' do
-        comment_content = 'Very good information'
         login(email: @user.email, password: 'An4r!s+Q')
 
         click_on 'Blog'
 
         click_on 'Read more...'
 
-        assert_no_text comment_content
+        assert_no_text @comment_content
 
         # Scroll down to activate the lazy loaded comments.
         # Sleep is necessary because scroll is async.
@@ -45,11 +44,11 @@ class CommentsTests < ApplicationSystemTestCase
 
         click_on 'Reply'
 
-        find('.comment-reply-input').set(comment_content)
+        find('.comment-reply-input').set(@comment_content)
 
         click_on 'Post Reply'
 
         assert_text 'Comment posted successfully.'
-        assert_text comment_content
+        assert_text @comment_content
     end
 end
