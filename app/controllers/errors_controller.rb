91f608915e4f3adfe3ffc3ac51abba80399465ec
Louvens Raphael <hardow2011@outlook.com>
2025-03-23 17:58:38 -0400

    wip: report 404 422 500 503 to error subscriber

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,10 @@
 class ErrorsController < ApplicationController
     def show
+        Rails.error.handle(severity: :warning) do
+            method = request.method
+            path = request.original_url
+            raise ActionController::RoutingError.new("No route matches [#{method}] \"/#{path}\"")
+        end
         @status_code = params[:code] || 500
     end
 end
