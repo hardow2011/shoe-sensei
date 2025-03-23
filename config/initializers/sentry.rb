43dbcd55ec9eb589e7bbbcd34134e7ca47343eed
Louvens Raphael <hardow2011@outlook.com>
2025-03-23 18:20:01 -0400

    wip: Sentry ignoring ctionController::RoutingError errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,7 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.dsn = Rails.application.credentials.dig(:sentry_dsn)
   config.enable_tracing = true
+  config.excluded_exceptions = []
   # TODO: find out what those values are
   config.traces_sample_rate = 1.0
   config.profiles_sample_rate = 1.0
