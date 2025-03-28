ab8b97db8a2d5f3242c6d183579e2d3ced3dd8d7
Louvens Raphael <hardow2011@outlook.com>
2025-03-28 13:35:40 -0400

    wip: comments compact button

--- app/javascript/comment_interactivity.js
+++ app/javascript/comment_interactivity.js
@@ -1,5 +1,15 @@
 window.minimizeComment = function minimizeComment(event) {
     const commentsToggleBar = event.target
     const repliesList = commentsToggleBar.closest('.replies-wrapper')
-    repliesList.classList.toggle('is-compacted')
+    if (repliesList.classList.contains('is-compacted')) {
+        repliesList.classList.add('is-opened')
+        repliesList.classList.remove('is-compacted')
+    } else {
+        repliesList.classList.add('is-compacted')
+        repliesList.classList.remove('is-opened')
+    }
+}
+
+window.toggleReplyCountbutton = function toggleReplyCountbutton(button) {
+    button.style.display = "none";
 }
