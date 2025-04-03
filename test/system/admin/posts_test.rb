9007b2c0b4ed788ff3d1cedb2774920265189654
Louvens Raphael <hardow2011@outlook.com>
2025-04-02 20:50:12 -0400

    refactor: consolidate test method

--- test/system/admin/posts_test.rb
+++ test/system/admin/posts_test.rb
@@ -156,33 +156,6 @@ class Admin::PostsTest < Admin::AdminSystemTestCase
 
     page.scroll_to(0, 5000)
 
-    within('.comments') do
-      assert_selector 'li.is-active', text: 'Published'
-      assert_selector 'li:not(.is-active)', text: 'Deleted'
-
-      published_comments.each do |c|
-          assert_text ActionController::Base.helpers.strip_tags(c.content)
-          assert_text "Posted on: #{I18n.l(c.created_at, format: :long)}"
-          if c.parent_comment
-              assert_text "Replying to: Comment ##{c.parent_comment.id}"
-          end
-          assert_text "Under: Post ##{c.post.id}"
-      end
-
-      click_on 'Deleted'
-
-      assert_selector 'li:not(.is-active)', text: 'Published'
-      assert_selector 'li.is-active', text: 'Deleted'
-
-      deleted_comments.each do |c|
-          assert_text I18n.t('comment.deleted')
-          assert_text "Posted on: #{I18n.l(c.created_at, format: :long)}"
-          if c.parent_comment
-              assert_text "Replying to: Comment ##{c.parent_comment.id}"
-          end
-          assert_text "Under: Post ##{c.post.id}"
-      end
-    end
-
+    assert_comments(published_comments, deleted_comments)
   end
 end
--- test/system/admin/users_test.rb
+++ test/system/admin/users_test.rb
@@ -62,35 +62,7 @@ class Admin::UsersTest < Admin::AdminSystemTestCase
         assert_button 'Delete User'
 
         # asserting user comments
-
-        within('.comments') do
-            assert_selector 'li.is-active', text: 'Published'
-            assert_selector 'li:not(.is-active)', text: 'Deleted'
-    
-            published_comments.each do |c|
-                assert_text ActionController::Base.helpers.strip_tags(c.content)
-                assert_text "Posted on: #{I18n.l(c.created_at, format: :long)}"
-                if c.parent_comment
-                    assert_text "Replying to: Comment ##{c.parent_comment.id}"
-                end
-                assert_text "Under: Post ##{c.post.id}"
-            end
-    
-            click_on 'Deleted'
-    
-            assert_selector 'li:not(.is-active)', text: 'Published'
-            assert_selector 'li.is-active', text: 'Deleted'
-    
-            deleted_comments.each do |c|
-                assert_text I18n.t('comment.deleted')
-                assert_text "Posted on: #{I18n.l(c.created_at, format: :long)}"
-                if c.parent_comment
-                    assert_text "Replying to: Comment ##{c.parent_comment.id}"
-                end
-                assert_text "Under: Post ##{c.post.id}"
-            end
-        end
-
+        assert_comments(published_comments, deleted_comments)
     end
 
 end
