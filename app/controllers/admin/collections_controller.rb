884df960aca3af81e49872446b2ecdab68b80927
Louvens Raphael <hardow2011@outlook.com>
2025-04-03 00:47:44 -0400

    wip: turbo refactor

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -1,13 +1,8 @@
 class Admin::CollectionsController < Admin::AdminController
   before_action :set_collection, only: %i[show edit update destroy]
+  before_action :set_collections, only: %i[index]
 
   def index
-    brand = Brand.get_by_handle(params[:brand_id])
-    if brand
-      @collections = brand.collections if brand
-      return
-    end
-    @collections = collections_by_default_order
   end
 
   def show
@@ -23,11 +18,12 @@ class Admin::CollectionsController < Admin::AdminController
     @collection = Collection.new(collection_params)
 
     if @collection.save
+      set_collections
       respond_to do |format|
         format.html { redirect_to admin_collections_path, notice: 'Collection was created successfully.' }
         format.turbo_stream do
           flash.now[:notice] = 'Collection was created successfully.'
-          @collections = Collection.where(brand: @collection.brand).order(:name)
+          # @collections = Collection.where(brand: @collection.brand).order(:name)
         end
       end
     else
@@ -49,23 +45,11 @@ class Admin::CollectionsController < Admin::AdminController
 
   def destroy
     @collection.destroy
-    # If the request comes from the collections index page, then
-    # repopulate with all collection after deletion
-    if request.params["deleted_from_index"] == "true"
-      collections = collections_by_default_order
-    # Otherwise, the collection deletion request comes from the brand page
-    # Then, return only the collection belonging to that brand 
-    else
-      collections = Collection.where(brand: @collection.brand).order(:name)
-    end
+    set_collections
     respond_to do |format|
       format.html { redirect_to admin_collections_path, notice: 'Collection was destroyed successfully.' }
       format.turbo_stream do
         flash.now[:notice] = 'Collection was destroyed successfully.'
-        @collections = collections
-        unless request.params["deleted_from_index"] == "true"
-          @brand = @collection.brand
-        end
       end
     end
   end
@@ -82,6 +66,16 @@ class Admin::CollectionsController < Admin::AdminController
 
   def collections_by_default_order
     Collection.order(created_at: :desc)
+  end
+
+  def set_collections
+    selected_brand = params['brand_id'].present? ? Brand.find(params[:brand_id]) : nil
+
+    if selected_brand
+      @collections = selected_brand.collections
+    else
+      @collections = Collection.order(created_at: :desc)
+    end
   end
 
 end
