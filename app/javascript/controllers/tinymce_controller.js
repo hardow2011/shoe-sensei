0d55a85740a06502a085bce934042b64e466be7f
Louvens Raphael <hardow2011@outlook.com>
2025-04-30 05:02:26 -0400

    feat: added comments

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -2,37 +2,51 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tinymce"
 export default class extends Controller {
-  // the form target will be the form element surrounding the tinyMCE field
-  // the field target will be the tinyMCE field itself
+  // The form target will be the form element surrounding the tinyMCE field
+  // The field target will be the tinyMCE field itself
   static targets = ['form', 'field']
 
   connect() {
   }
 
+  // Every time a fieldTarget (i.e. a tinyMCE field) is added to the page,
+  // the fieldTargetConnected will run.
+  // This funtion is used to initialize a tinyMCE using a specific selector and configuration.
   fieldTargetConnected() {
-    // The random ID every time is necessary because TinyMCERails.initialize won't...
-    // initialize the same ID more than once
+    // this.fieldTargets get every element with the data-tinymce-target="field" attribute
     this.fieldTargets.forEach((field, _) => {
-      const fieldId = 'time-mce-field-' + crypto.randomUUID();
-      const config = field.dataset.tinymceConfig;
+      // The random ID every time is necessary because it is the selector that TinyMCERails.initialize
+      // will use to initialize TinyMCE in a specific field.
+      // If the field come from a Turbo Stream, this will also help re-initialize the field.
+      const fieldId = 'timemce-field-' + crypto.randomUUID();
+      
+      // The config to be used for the field retrieved from the data-tinymce-config attribute.
+      // If not assiged, then it will default to 'default'.
+      const config = field.dataset.tinymceConfig || 'default';
   
+      // The random ID is assigned to the tinyMCE field.
       field.id = fieldId;
 
-      console.log(field);
-      
-      
+      // tinyMCE is initialized to an element using the random ID as selector
       TinyMCERails.initialize(config, {
         selector: `#${fieldId}`
       });
     })
   }
 
+  // Get all tinyMCE fields in a form (identified by the attribute data-tinymce-target="field")
   get tinymceFields() {
-    return this.element.querySelectorAll('.tinymce');
+    return this.element.querySelectorAll('[data-tinymce-target="field"]');
   }
 
+  // This function will be called on form submission.
+  // It's used to set a hidden field with the value of the tinyMCE field.
+  // This is so the tinyMCE field value can be properly passed to the controller because the
+  // tinyMCE field is a div, and divs do not get sent with form submissions.
   setHiddenField() {
     this.tinymceFields.forEach((field, index) => {
+      // It is important to assign the correct name attribute to the tinyMCE field,
+      // as it will be used for the hidden field
       const fieldName = field.getAttribute('name');
       
       let hiddenTextAreaInput = document.createElement('textarea');
