c63ada9a51639d53f5c6a4f2fc4a6b00fe31d8d5
Louvens Raphael <hardow2011@outlook.com>
2025-05-02 03:09:33 -0400

    wip: N+1 query problem from search bar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,25 @@
 class SearchController < ApplicationController
     def autocomplete
-        posts = Post.search(search_params, fields: ["title_#{locale}", "content_#{locale}", "overview_#{locale}"], match: :word_start)
-        brands = Brand.search(search_params, fields: [:name, "overview_#{locale}"], match: :word_start)
-        collections = Collection.search(search_params, fields: [:name, "overview_#{locale}"], match: :word_start)
-        Searchkick.multi_search([posts, brands, collections])
+        # "load: false" to get results from search server only, without calling the Database
+        posts_search = Post.search(search_params, fields: ["title_#{locale}", "content_#{locale}", "overview_#{locale}"], match: :word_start, load: false)
+        brands_search = Brand.search(search_params, fields: [:name, "overview_#{locale}"], match: :word_start, load: false)
+        collections_search = Collection.search(search_params, fields: [:name, "overview_#{locale}"], match: :word_start, load: false)
+        Searchkick.multi_search([posts_search, brands_search, collections_search])
 
-        responses = collections.response['hits']['hits'] + posts.response['hits']['hits'] + brands.response['hits']['hits']
-        results = posts.to_a + brands.to_a + collections.to_a
+        posts_ids = posts_search.map(&:id)
+        brands_ids = brands_search.map(&:id)
+        collections_ids = collections_search.map(&:id)
+
+        # Now we call the DB to retrieve the objects, alongside with_attached_logo for brands and .includes(models: :image_attachment) for collections
+        # This avoids the N+1 query problem
+        posts = Post.where(id: posts_ids)
+        # eager load brands logos
+        brands = Brand.where(id: brands_ids).with_attached_logo
+        # eager load model images from collections
+        collections = Collection.where(id: collections_ids).includes(models: :image_attachment)
+
+        responses = collections_search.response['hits']['hits'] + posts_search.response['hits']['hits'] + brands_search.response['hits']['hits']
+        results = posts + brands + collections
 
         # The sort matches the id and class.name as substring and sorts by score
         results.sort_by! { |result| - responses.find { |response| match_result_response(result, response) }['_score'] }
