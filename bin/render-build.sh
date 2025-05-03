0c5acbe646fbfb9ef63ca0e61dd37a61c168f53c
Louvens Raphael <hardow2011@outlook.com>
2025-05-02 23:45:24 -0400

    wip: fixing prod queue config

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-bundle exec rails db:prepare
-
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
--- bin/render-pre-deploy.sh
+++ bin/render-pre-deploy.sh
@@ -4,3 +4,6 @@ set -o errexit
 
 echo "running: bundle exec rails db:migrate"
 bundle exec rails db:migrate
+
+echo "running: bundle exec rails db:prepare"
+bundle exec rails db:prepare
