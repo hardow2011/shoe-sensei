19f61f1134a99955eeafa4186b173ddd1b2fac21
Louvens Raphael <hardow2011@outlook.com>
2025-05-08 00:17:12 -0400

    wip: disable solid_queue

--- bin/render-pre-deploy.sh
+++ bin/render-pre-deploy.sh
@@ -4,6 +4,3 @@ set -o errexit
 
 echo "running: bundle exec rails db:migrate"
 bundle exec rails db:migrate
-
-echo "running: bundle exec rails db:prepare for the queue database"
-bundle exec rails db:prepare
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.cache_store = :redis_cache_store, { url: ENV["REDIS_CACHE_STORE_URL"] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  # config.active_job.queue_adapter = :solid_queue
+  # config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "footwearapp_production"
 
   config.action_mailer.perform_caching = false
